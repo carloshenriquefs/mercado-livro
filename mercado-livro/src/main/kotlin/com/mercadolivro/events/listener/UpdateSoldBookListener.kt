73cf02e2fad3bff8a5474e4411fd5c4a2033cf88
Carlos Henrique <ichfs9108@gmail.com>
2024-06-13 14:44:45 -0300

    feat: Deixando nossos Eventos Assincronos

--- mercado-livro/src/main/kotlin/com/mercadolivro/events/listener/UpdateSoldBookListener.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/events/listener/UpdateSoldBookListener.kt
@@ -4,6 +4,7 @@ import com.mercadolivro.events.PurchaseEvent
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -12,8 +13,10 @@ class UpdateSoldBookListener(
         private val bookService: BookService
 ) {
 
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent) {
+        println("Atualizando status do livro")
         bookService.purchase(purchaseEvent.purchaseModel.books)
     }
 }
