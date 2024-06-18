4006b1c30642f5fcf1af66d43c73bb7b1e6c0f57
Carlos Henrique <ichfs9108@gmail.com>
2024-06-18 15:58:06 -0300

    feat: Implementar o filter de Autorizacao

--- mercado-livro/src/main/kotlin/com/mercadolivro/security/JwtUtil.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/security/JwtUtil.kt
@@ -1,9 +1,12 @@
 package com.mercadolivro.security
 
+import com.mercadolivro.exception.AuthenticationException
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.lang.Exception
 import java.util.*
 
 @Component
@@ -22,4 +25,25 @@ class JwtUtil {
                 .signWith(SignatureAlgorithm.HS512, secret!!.toByteArray())
                 .compact()
     }
+
+    fun isValidToken(token: String): Boolean {
+        val claims = getClaims(token)
+        if(claims.subject == null || claims.expiration == null || Date().after(claims.expiration)) {
+            return false
+        }
+        return true
+    }
+
+    private fun getClaims(token: String): Claims {
+        try {
+            return Jwts.parser().setSigningKey(secret!!.toByteArray()).parseClaimsJws(token).body
+        } catch (ex : Exception) {
+            throw AuthenticationException("Token Inválido", "999")
+        }
+    }
+
+    fun getSubject(token: String): String {
+        return getClaims(token).subject
+    }
+
 }
