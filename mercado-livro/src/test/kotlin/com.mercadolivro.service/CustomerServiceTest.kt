573e7a2b569fbd0e4dbc14099b7797a217d14f4b
Carlos Henrique <ichfs9108@gmail.com>
2024-06-21 13:51:17 -0300

    feat: Utilizando Slot

--- mercado-livro/src/test/kotlin/com.mercadolivro.service/CustomerServiceTest.kt
+++ mercado-livro/src/test/kotlin/com.mercadolivro.service/CustomerServiceTest.kt
@@ -4,6 +4,7 @@ import com.mercadolivro.enums.CustomerStatus
 import com.mercadolivro.enums.Errors
 import com.mercadolivro.enums.Role
 import com.mercadolivro.exception.NotFoundException
+import com.mercadolivro.helper.buildCustomer
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
 import io.mockk.every
@@ -214,17 +215,5 @@ class CustomerServiceTest {
     }
 
 
-    fun buildCustomer(
-            id: Int? = null,
-            name: String = "customer name",
-            email: String = "${UUID.randomUUID()}@email.com",
-            password: String = "password"
-    ) = CustomerModel(
-            id = id,
-            name = name,
-            email = email,
-            status = CustomerStatus.ATIVO,
-            password = password,
-            roles = setOf(Role.CUSTOMER)
-    )
+
 }
